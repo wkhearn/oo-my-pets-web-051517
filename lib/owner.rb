6cb246fd88568eea2512f833b47830ee8559339f
wkhearn <wkhearn@gmail.com>
2017-05-19 20:47:04 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,70 @@
+require "pry"
+
 class Owner
-  # code goes here
+  attr_accessor :name,:pets
+  attr_reader :species
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @species = "human"
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def pets
+    @pets
+  end
+
+  def buy_fish(name)
+    self.pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    self.pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    self.pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    self.pets[:dogs].map {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    self.pets[:cats].map {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    self.pets[:fishes].map {|fish| fish.mood = "happy"}
+  end
+
+  def sell_pets
+    self.pets[:dogs].map {|o| o.mood = "nervous"}
+    self.pets[:cats].map {|o| o.mood = "nervous"}
+    self.pets[:fishes].map {|o| o.mood = "nervous"}
+    self.pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def list_pets
+    "I have #{self.pets[:fishes].length} fish, #{self.pets[:dogs].length} dog(s), and #{self.pets[:cats].length} cat(s)."
+  end
+
 end
